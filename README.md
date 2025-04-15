8ba28b69b970b384e6b48cf1f5c2e64fd9258965
vinayakashwin1010 <vinayakashwin1010@gmail.com>
2025-04-15 10:32:55 +0530

    project completed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # EC2-Auto-Backup-Tool
+
+This script automates snapshot backups of EC2 volumes for instances tagged with backup:true
+
+## Featrues
+
+- Backs up running EC2 instances
+- Create EBS Snapshots of attached volumes
+- Auto description with timestamp
+
+## How to use
+
+python backup.py --desc "daily backup"
---
+++ backup.py
@@ -0,0 +1,50 @@
+import boto3
+import datetime
+import argparse
+
+def create_snapshot(instance_id, description):
+    ec2 = boto3.client('ec2')
+    volumes = ec2.describe_instances(InstanceIds=[instance_id])
+
+    for reservation in volumes['Reservations']:
+        for instance in reservation['instance']:
+            for device in instance['BlockDeviceMappings']:
+                volume_id = device['Ebs']['VolumeId']
+                print(f"Creating snapshot for volume: {volume_id}")
+                snapshot = ec2.create_snapshot(
+                    VolumeId=volume_id,
+                    Description=description
+                )
+                print(f"Snapshot created:{snapshot['SnapshotId']}")
+
+def get_backup_instances(tag_key='backup', tag_value='true'):
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_instances(
+        Filters=[
+            {'Name': f'tag:[tag_key]', 'Values': [tag_value]},
+            {'Name': 'instance-state-name', 'Values': ['running']}
+        ]
+    )
+    instances = []
+    for reservation in response['Reservations']:
+        for instance in reservation['instances']:
+            instances.append(instance['InstanceId'])
+    return instances
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="EC2 Auto Snapshot Tool")
+    parser.add_argument('--desc', type=str, help="snapshot description", default="Automated backup")
+    args = parser.parse_args()
+
+    today = datetime.datetime.now().strftime("%y-%m-%d")
+    desc = f"{args.desc} - {today}"
+
+    print("fetching instances with backup=true tag...")
+    instance_ids = get_backup_instances()
+
+    if not instance_ids:
+        print("No backup tagged instances found")
+    else:
+        print(f"Instances found: {instance_ids}")
+        for instance_id in instance_ids:
+            create_snapshot(instance_id, desc)
